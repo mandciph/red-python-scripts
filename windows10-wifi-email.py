7e5fd66a23be4f58352d74b2cbb4d84cfeadff98
mandciph <34348830+mandciph@users.noreply.github.com>
2022-08-28 07:20:53 -0400

    Update windows10-wifi-email.py

--- windows10-wifi-email.py
+++ windows10-wifi-email.py
@@ -68,6 +68,6 @@ with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     # Connect securely to server
     smtp.starttls()
     # Login using username and password to dummy email. Remember to set email to allow less secure apps if using Gmail
-    smtp.login("login_name", "password")
+    smtp.login("mikemuffo@gmail.com", "password")
     # Send email.
     smtp.send_message(email)
